e73185322cf7e22df412e3b984beae199348611e
Parncncd <69336494+Parncncd@users.noreply.github.com>
2020-08-22 00:55:47 +0700

    Add output 1

--- Challenge_loop_if_basicmath/Chal1.cpp
+++ Challenge_loop_if_basicmath/Chal1.cpp
@@ -4,46 +4,47 @@
 
 int main()
 {
-	double num1, num2, sum = 0, n, Average, SUM = 0, SD;
-	scanf("%lf %lf", &num1, &num2);
-	
+    double num1, num2, sum = 0, n, Average, SUM = 0, SD;
+    scanf("%lf %lf", &num1, &num2);
 
-	if (num1 > num2) {
-		for (double i = num1;i >= num2;i--) {
 
-			sum += i;
-		}
-		n = num1 - num2 + 1;
-	}
-	else if (num1 < num2) {
-		for (double j = num1;j <= num2;j++) {
+    if (num1 > num2) {
+        for (int i = num1; i >= num2; i--) {
+            printf("%d ", i);
+            sum += i;
+        }
+        printf("\n");
+        n = num1 - num2 + 1;
+    }
+    else if (num1 < num2) {
+        for (int j = num1; j <= num2; j++) {
+            printf("%d ", j);
+            sum += j;
+            n = num2 - num1 + 1;
+        }
+        printf("\n");
+    }
+    else {
+    }
+    Average = sum / n;
+    printf("Average : %.1lf", Average);
+    if (num1 > num2) {
+        for (double k = num1; k >= num2; k--) {
 
-			sum += j;
-			n = num2 - num1 + 1;
-		}
-	}
-	else {
-	}
-	Average = sum / n;
-	printf("Average : %.2lf", Average);
-	if (num1 > num2) {
-		for (double k = num1;k >= num2;k--) {
+            SUM += (k - Average) * (k - Average);
+        }
+    }
+    else if (num1 < num2) {
+        for (double l = num1; l <= num2; l++) {
 
-		SUM += (k - Average) * (k - Average);
-	}
-	}
-	else if (num1 < num2) {
-		for (double l = num1;l <= num2;l++) {
+            SUM += (l - Average) * (l - Average);
+        }
+    }
+    else {
+    }
 
-		SUM += (l - Average) * (l - Average);
-		}
-	}
-	else {
-	}
-	
-	SD = sqrt(SUM / (n-1));
-	printf("\nSD : %.2lf\n", SD);
-	
-	return 0;
+    SD = sqrt(SUM / (n - 1));
+    printf("\nSD : %.2lf\n", SD);
+
+    return 0;
 }
-
